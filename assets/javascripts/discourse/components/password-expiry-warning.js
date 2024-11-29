c26aa7010d2e3123c128e59fdd67df1dd56ce4d8
David Taylor <david@taylorhq.com>
2024-11-29 15:42:11 +0000

    DEV: Convert to native class syntax (#21)

--- assets/javascripts/discourse/components/password-expiry-warning.js
+++ assets/javascripts/discourse/components/password-expiry-warning.js
@@ -1,33 +1,33 @@
 import Component from "@ember/component";
+import { action } from "@ember/object";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  changing: false,
-  changed: false,
-  sentToEmail: "",
+export default class PasswordExpiryWarning extends Component {
+  changing = false;
+  changed = false;
+  sentToEmail = "";
 
   @discourseComputed("currentUser.password_expires_at")
   daysLeft(passwordExpiresAt) {
     return Math.ceil(moment(passwordExpiresAt).diff(moment(), "days", true));
-  },
+  }
 
   @discourseComputed("currentUser.password_expires_at")
   expired(passwordExpiresAt) {
     return moment().isAfter(moment(passwordExpiresAt));
-  },
+  }
 
-  actions: {
-    changePassword() {
-      this.set("changing", true);
-      this.currentUser.findDetails().then((user) => {
-        this.set("sentToEmail", user.email);
-        user
-          .changePassword()
-          .then(() => this.set("changed", true))
-          .catch(popupAjaxError)
-          .finally(() => this.set("changing", false));
-      });
-    },
-  },
-});
+  @action
+  changePassword() {
+    this.set("changing", true);
+    this.currentUser.findDetails().then((user) => {
+      this.set("sentToEmail", user.email);
+      user
+        .changePassword()
+        .then(() => this.set("changed", true))
+        .catch(popupAjaxError)
+        .finally(() => this.set("changing", false));
+    });
+  }
+}
